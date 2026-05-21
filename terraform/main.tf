76c3956832791b81090f2bf866fad9bb56030f60
Pragati2708 <pragatisingh2708@gmail.com>
2026-05-21 23:21:37 +0530

    "port access update"

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,6 +21,18 @@ resource "aws_security_group" "portfolio_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+  from_port   = 9090
+  to_port     = 9090
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   egress {
     from_port   = 0
